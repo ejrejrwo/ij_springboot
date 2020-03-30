d3ac35c6a89c216d8d7266ae8f528ee4586b0771
ejrejrwo <ejrejrwo@naver.com>
2020-03-31 02:05:56 +0900

    롬복 사용

---
+++ src/test/java/com/jojoldu/book/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,22 @@
+package com.jojoldu.book.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class HelloResponseDtoTest {
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        // assertThat - 테스트 검증 메소드 : 검증하고 싶은 대상을 메소드 인자로 받습니다.
+        // isEqualTo - 동등 비교 메소드 : assertThat에 있는 값과 isEqualTo의 값을 비교해서 같을 때만 성공입니다.
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
